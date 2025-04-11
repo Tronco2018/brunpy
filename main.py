7585c0b0f7a60a7d512ebb519c57cef027b1d3ca
Tronco2018 <tronco2018dev@gmail.com>
2025-04-11 16:53:18 +0200

    Modified main.py
    Corretto errore di presa di input forzando un tipo 'float' invece che un string

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ def somma(a, b):
     #scrivi un codice che inserisca a + b dentro risultato
     #....
     return risultato
-prim = input("Inserisci il primo numero: ")
-sec = input("Inserisci il secondo numero: ")
-somma(prim, sec)
-print("Risultato:" + str(risultato))
+prim = float(input("Inserisci il primo numero: "))
+sec = float(input("Inserisci il secondo numero: "))
+
+print("Risultato:" + str(somma(prim, sec)))
