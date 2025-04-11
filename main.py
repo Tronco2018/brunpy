669deb8766f98a4703a0adc6b8a8efe52e3a3422
BrunazzoDev <brunazzo.dev@gmail.com>
2025-04-11 17:05:26 +0200

    ho modificato il main.py
    ho finito la fucking calcolatrice (si spera)

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 
 def somma(a, b):
     #scrivi un codice che inserisca a + b dentro risultato
-    #....
+    risultato = a + b
     return risultato
-prim = input("Inserisci il primo numero: ")
-sec = input("Inserisci il secondo numero: ")
-somma(prim, sec)
-print("Risultato:" + str(risultato))
+prim = float(input("Inserisci il primo numero: "))
+sec = float(input("Inserisci il secondo numero: "))
+
+print("Risultato:" + str(somma(prim, sec)))
