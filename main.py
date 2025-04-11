33a72899a990599378db322ae1efa026ef652426
Tronco2018 <tronco2018dev@gmail.com>
2025-04-11 16:46:32 +0200

    Modified main.py
    Aggiuta struttura di funzione somma, input utente per valori da sommare, chiamata di funzione somma() e stampa risultato

---
+++ main.py
@@ -0,0 +1,9 @@
+
+def somma(a, b):
+    #scrivi un codice che inserisca a + b dentro risultato
+    #....
+    return risultato
+prim = input("Inserisci il primo numero: ")
+sec = input("Inserisci il secondo numero: ")
+somma(prim, sec)
+print("Risultato:" + str(risultato))
